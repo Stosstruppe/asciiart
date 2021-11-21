076c32631d96f128ee6baff356ece915e759d62b
Stosstruppe <nx125@live.jp>
2021-11-22 07:28:55 +0900

    a

---
+++ C#/asciiart2.cs
@@ -0,0 +1,34 @@
+// asciiart Visual C#
+/*
+path %path%;C:\Windows\Microsoft.NET\Framework64\v4.0.30319
+csc asciiart.cs
+*/
+
+using System;
+
+class Program
+{
+	static void Main()
+	{
+		for (var y = -12; y <= 12; y++) {
+			for (var x = -39; x <= 39; x++) {
+				var ca = x * 0.0458;
+				var cb = y * 0.08333;
+				var a = ca;
+				var b = cb;
+				var s = " ";
+				for (var i = 0; i <= 15; i++) {
+					var t = a * a - b * b + ca;
+					b = 2 * a * b + cb;
+					a = t;
+					if ((a * a + b * b) > 4) {
+						s = i.ToString("X");
+						break;
+					}
+				}
+				Console.Write(s);
+			}
+			Console.WriteLine();
+		}
+	}
+}
